76fe10a2244162a4bc7c2b266a8bed6a4522501d
Yunqi Zhao <yunqizhao01@gmail.com>
2024-07-07 21:01:30 -0400

    Fix issues

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import IMG from './IMG.png';
 import subIMG from './subIMG.png';
+import { Link as ScrollLink, Element } from 'react-scroll';
 
 function Home() {
   const buttonStyle = {
@@ -15,60 +16,66 @@ function Home() {
     alignItems: 'center',
     justifyContent: 'center',
     textDecoration: 'none',
+    cursor: 'pointer'
   };
   return (
-    
     <div className='homecontent'>
       <div style={{ display: 'flex', justifyContent: 'center', marginLeft: '80px', marginRight: '80px' }}>
-        <Button href="#update" style={buttonStyle}>Update</Button >
-        <Button href="#release" style={buttonStyle}>Release</Button >
-        <Button href="#review" style={buttonStyle}>Review</Button >
+        <ScrollLink to="update" smooth={true} duration={500} style={buttonStyle}>Update</ScrollLink>
+        <ScrollLink to="release" smooth={true} duration={500} style={buttonStyle}>Release</ScrollLink>
+        <ScrollLink to="review" smooth={true} duration={500} style={buttonStyle}>Review</ScrollLink>
       </div>
 
-      <section id="update">
-        <h1 style={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>Update</h1>
-        <div className='titleImage'>
-          <img src={IMG} alt="img" />
-        </div>
-        <h3 style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry</h3>
-        <div className='subImage'>
-          <img src={subIMG} style={{ marginRight: '150px' }} alt="img" />
-          <img src={subIMG} style={{ marginLeft: '150px' }} alt="img" />
-        </div>
+      <Element name="update">
+        <section id="update">
+          <h1 style={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>Update</h1>
+          <div className='titleImage'>
+            <img src={IMG} alt="img" />
+          </div>
+          <h3 style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </h3>
+          <div className='subImage'>
+            <img src={subIMG} style={{ marginRight: '150px' }} alt="img" />
+            <img src={subIMG} style={{ marginLeft: '150px' }} alt="img" />
+          </div>
+        </section>
+      </Element>
 
-      </section>
+      <Element name="release">
+        <section id="release">
+          <h1 style={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>Release</h1>
+          <div className='titleImage'>
+            <img src={IMG} alt="img" />
+          </div>
+          <h3 style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </h3>
+          <div className='subImage'>
+            <img src={subIMG} style={{ marginRight: '150px' }} alt="img" />
+            <img src={subIMG} style={{ marginLeft: '150px' }} alt="img" />
+          </div>
+        </section>
+      </Element>
 
-
-      <section id="release">
-        <h1 style={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>Release</h1>
-        <div className='titleImage'>
-          <img src={IMG} alt="img" />
-        </div>
-        <h3 style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry</h3>
-        <div className='subImage'>
-          <img src={subIMG} style={{ marginRight: '150px' }} alt="img" />
-          <img src={subIMG} style={{ marginLeft: '150px' }} alt="img" />
-        </div>
-      </section>
-
-      <section id="review">
-        <h1 style={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>Review</h1>
-        <div className='titleImage'>
-          <img src={IMG} alt="img" />
-        </div>
-        <h3 style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry</h3>
-        <div className='subImage'>
-          <img src={subIMG} style={{ marginRight: '150px' }} alt="img" />
-          <img src={subIMG} style={{ marginLeft: '150px' }} alt="img" />
-        </div>
-      </section>
-
-
+      <Element name="review">
+        <section id="review">
+          <h1 style={{ display: 'flex', justifyContent: 'center', margin: '50px' }}>Review</h1>
+          <div className='titleImage'>
+            <img src={IMG} alt="img" />
+          </div>
+          <h3 style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </h3>
+          <div className='subImage'>
+            <img src={subIMG} style={{ marginRight: '150px' }} alt="img" />
+            <img src={subIMG} style={{ marginLeft: '150px' }} alt="img" />
+          </div>
+        </section>
+      </Element>
     </div>
   );
 }
 
 export default Home;
+
