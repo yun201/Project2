f384cd6292a6fef19d82a4a42e23cdf457a9a6b5
Yunqi Zhao <yunqizhao01@gmail.com>
2024-07-07 20:10:46 -0400

    Fix routing issues

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,17 @@
+import { Link } from 'react-router-dom';
 import logo from './logo.png';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <img src={logo} width={70} height={70}  alt="Logo" />
+            <img src={logo} width={70} height={70} alt="Logo" />
             <h1 style={{marginLeft: 10}}>GameSword</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/games">Games</a>
-                <a href="/contact">Contact Us</a>
+                <Link to="/">Home</Link>
+                <Link to="/games">Games</Link>
+                <Link to="/contact">Contact Us</Link>
             </div>
         </nav>
-        
      );
 }
  
